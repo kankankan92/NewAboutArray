d504472d4c51d655a62aa4d78460bb3e99c9c96c
nastya <kankankan92@mail.ru>
2015-06-19 00:57:36 +0400

    Создали скелет.

---
+++ src/main/java/ru/kourilenko/MyArray.java
@@ -0,0 +1,29 @@
+package ru.kourilenko;
+
+
+public class MyArray {
+    public void add(Object object){
+
+    }
+    public void add(Object object, int position){
+
+    }
+    public void remove(){
+
+    }
+    public void removw(int position){
+
+    }
+    public Object get(int position){
+        return null;
+    }
+    public int size(){
+        return 0;
+    }
+    public boolean isEmpty(){
+        return true;
+    }
+    public void clear(){
+
+    }
+}
