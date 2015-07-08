3958b97e0baf283ec69b822b7415b1950038a18f
nastya <kankankan92@mail.ru>
2015-07-08 11:22:54 +0400

    Реализация методов add и get.

--- src/main/java/ru/kourilenko/Main.java
+++ src/main/java/ru/kourilenko/Main.java
@@ -1,7 +1,13 @@
 package ru.kourilenko;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Integer> al= new ArrayList<Integer>();
+        al.add(0, 5);
+
+        MyArray  ma = new MyArray();
 
     }
 }
--- src/main/java/ru/kourilenko/MyArray.java
+++ src/main/java/ru/kourilenko/MyArray.java
@@ -2,28 +2,65 @@ package ru.kourilenko;
 
 
 public class MyArray {
-    public void add(Object object){
+
+    private int size;
+    private static final int INIT_SIZE = 50;
+    private Object[] myarray;
+
+    public MyArray() {
+        myarray = new Object[INIT_SIZE];
+        size = 0;
+    }
+
+    public void add(Object object) {
+        if (size == myarray.length) {
+            Object[] newmyarray = new Object[myarray.length + INIT_SIZE];
+            System.arraycopy(myarray, 0, newmyarray, 0, myarray.length);
+            myarray = newmyarray;
+        }
+        myarray[size] = object;
+        size++;
+    }
+
+    public void add(int position, Object object) {
+        if (position < 0 || position > myarray.length) {
+            throw new IllegalArgumentException("Некорректное значение переменной position");
+        }
+        if (size == myarray.length) {
+            Object[] newmyarray = new Object[myarray.length + INIT_SIZE];
+            System.arraycopy(myarray, 0, newmyarray, 0, myarray.length);
+            myarray = newmyarray;
+        }
+        System.arraycopy(myarray, position, myarray, position + 1, myarray.length - position);
+        myarray[position] = object;
+        size++;
 
     }
-    public void add(Object object, int position){
+
+    public void remove() {
 
     }
-    public void remove(){
+
+    public void removw(int position) {
 
     }
-    public void removw(int position){
 
+    public Object get(int position) {
+        if (position < 0 || position > myarray.length) {
+            throw new IllegalArgumentException("Некорректное значение переменной position");
+        }
+        return myarray[position];
     }
-    public Object get(int position){
-        return null;
+
+    public int size() {
+        return size;
     }
-    public int size(){
-        return 0;
-    }
-    public boolean isEmpty(){
+
+    public boolean isEmpty() {
         return true;
     }
-    public void clear(){
+
+    public void clear() {
 
     }
 }
